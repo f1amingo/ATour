a1b9feba87b47601a8d38b5b7c18e36e04f1cd4e
flamingo <zsjperiod@foxmail.com>
2019-12-14 16:13:38 +0800

    即使通信配置

--- app/src/main/java/com/sanwei/sanwei4a/App.kt
+++ app/src/main/java/com/sanwei/sanwei4a/App.kt
@@ -4,15 +4,25 @@ import android.annotation.SuppressLint
 import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.NotificationCompat
+import android.util.Log
 import com.facebook.drawee.backends.pipeline.Fresco
+import com.netease.nimlib.sdk.*
+import com.netease.nimlib.sdk.auth.AuthServiceObserver
+import com.netease.nimlib.sdk.auth.LoginInfo
+import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum
+import com.netease.nimlib.sdk.uinfo.UserInfoProvider
+import com.netease.nimlib.sdk.uinfo.model.UserInfo
+import com.netease.nimlib.sdk.util.NIMUtil
 import com.sanwei.sanwei4a.activity.SystemNotificationActivity
 import com.sanwei.sanwei4a.entity.po.Account
 import com.sanwei.sanwei4a.util.LogUtil
 import com.sanwei.sanwei4a.util.RequestUtil
 import org.jetbrains.anko.defaultSharedPreferences
+
 
 class App : Application() {
 
@@ -52,6 +62,50 @@ class App : Application() {
             override fun onActivityDestroyed(p0: Activity?) = Unit
 
         })
+
+        // SDK初始化（启动后台服务，若已经存在用户登录信息， SDK 将完成自动登录）
+        NIMClient.init(this, loginInfo(), options())
+        if (NIMUtil.isMainProcess(this)) {
+            // 注意：以下操作必须在主进程中进行
+            // 1、UI相关初始化操作
+            // 2、相关Service调用
+            NIMClient.getService(AuthServiceObserver::class.java).observeOnlineStatus(
+                    { status -> Log.d("App", status.name) }, true)
+        }
+    }
+
+    // 如果返回值为 null，则全部使用默认参数。
+    private fun options(): SDKOptions {
+        val options = SDKOptions()
+        options.appKey = "afcf5899f1d81c39a6780a7cbbf4b322"
+        // 如果将新消息通知提醒托管给 SDK 完成，需要添加以下配置。否则无需设置。
+//        val config = StatusBarNotificationConfig()
+//        config.notificationEntrance = WelcomeActivity; // 点击通知栏跳转到该Activity
+//        config.notificationSmallIconId = R.drawable.ic_stat_notify_msg;
+        // 配置是否需要预下载附件缩略图，默认为 true
+        options.preloadAttach = true
+        // 配置附件缩略图的尺寸大小。表示向服务器请求缩略图文件的大小
+        // 该值一般应根据屏幕尺寸来确定， 默认值为 Screen.width / 2
+//        options.thumbnailSize = ${ Screen.width } / 2;
+        // 用户资料提供者, 目前主要用于提供用户资料，用于新消息通知栏中显示消息来源的头像和昵称
+        options.userInfoProvider = object : UserInfoProvider {
+            override fun getUserInfo(account: String?): UserInfo? {
+                return null
+            }
+
+            override fun getAvatarForMessageNotifier(sessionType: SessionTypeEnum?, sessionId: String?): Bitmap? {
+                return null
+            }
+
+            override fun getDisplayNameForMessageNotifier(account: String?, sessionId: String?, sessionType: SessionTypeEnum?): String? {
+                return null
+            }
+        }
+        return options
+    }
+
+    private fun loginInfo(): LoginInfo? {
+        return LoginInfo("test1", "975320bfc31f73d3ad9067daeeff9712")
     }
 
     companion object {
@@ -91,7 +145,7 @@ class App : Application() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
                 createNotificationChannel()
 
-            mContext.getSystemService(NotificationManager::class.java).notify(notificationId, builder.build())
+            mContext.getSystemService(NotificationManager::class.java)!!.notify(notificationId, builder.build())
         }
 
         private fun createNotificationChannel() {
@@ -101,7 +155,7 @@ class App : Application() {
                         NotificationManager.IMPORTANCE_HIGH)
                 chanel.description = "芝士外脑即时通信服务"
                 val notificationManager = mContext.getSystemService(NotificationManager::class.java)
-                notificationManager.createNotificationChannel(chanel)
+                notificationManager!!.createNotificationChannel(chanel)
                 isChannelCreated = true
             }
         }
